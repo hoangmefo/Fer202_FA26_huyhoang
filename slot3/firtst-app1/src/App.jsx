d9741399c8bbd887c40f6fc16562231e22a14f39
hoangmefo <hoangmefo@example.com>
2026-09-14 09:01:24 +0700

    feat(slot3): add About component

---
+++ slot3/firtst-app1/src/App.jsx
@@ -0,0 +1,12 @@
+import About from './about'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <About />
+    </main>
+  )
+}
+
+export default App
---
+++ slot3/firtst-app1/src/about.jsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <section className="about">
+      <h2> About page </h2>
+
+      <p><strong>Họ tên:</strong> Đỗ Huy Hoàng</p>
+      <p><strong>Mã số sinh viên:</strong> DE191039</p>
+      <p><strong>Lớp:</strong> SE20B04</p>
+      <p><strong>Email:</strong> hoanghuydo07@gmail.com</p>
+    </section>
+  )
+}
